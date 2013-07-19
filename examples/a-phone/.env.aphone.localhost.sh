66166ed50a2f7f78deabbfbbfed8d0e104d088b6
Geoff Hollingworth <geoff.hollingworth@ericsson.com>
2013-07-19 11:32:45 -0700

    new app credentials

---
+++ examples/a-phone/.env.aphone.localhost.sh
@@ -0,0 +1,3 @@
+export CALLBACK_URL="http://localhost:5000/users/auth/att/callback"
+export ATT_CLIENT_ID="q0sguq2gqago1dl9tk2kadbwgesnmbyk"
+export ATT_CLIENT_SECRET="pt5dukjcxuvc2rzf0ieldjopsphrnjor"
