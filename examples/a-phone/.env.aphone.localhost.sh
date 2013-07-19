61c5605faa8a2e910b55b707c7e216976ceca041
Geoff Hollingworth <geoff.hollingworth@ericsson.com>
2013-07-19 11:38:42 -0700

    new aphone credentials for Aphone WCG Phone

--- examples/a-phone/.env.aphone.localhost.sh
+++ examples/a-phone/.env.aphone.localhost.sh
@@ -1,3 +1,3 @@
 export CALLBACK_URL="http://localhost:5000/users/auth/att/callback"
-export ATT_CLIENT_ID="q0sguq2gqago1dl9tk2kadbwgesnmbyk"
-export ATT_CLIENT_SECRET="pt5dukjcxuvc2rzf0ieldjopsphrnjor"
+export ATT_CLIENT_ID="gzdppnbwm4etkfb7wbbxdcdaa9evmgdf"
+export ATT_CLIENT_SECRET="bqacc90drofcg7737xzcy7lnkxwxa1jj"
